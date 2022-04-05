4155e1406ccc1eb689e10203e2824c5c051bcc4f
miketaonjo <79211048+miketaonjo@users.noreply.github.com>
2022-04-05 09:40:03 +0200

    Add files via upload

---
+++ libft/ft_memcmp.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcloarec <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/01 10:44:05 by mcloarec          #+#    #+#             */
+/*   Updated: 2022/04/01 12:48:16 by mcloarec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	i = 0;
+	while (n > 0 && (str1[i] != '\0' || str2[i] != '\0'))
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+		n--;
+	}
+	if (n == 0)
+		return (0);
+	return (str1[i] - str2[i]);
+}
