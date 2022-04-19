0fb5446a8a2c3403dfda2e34ee3d8ccb5aed1347
miketaonjo <79211048+miketaonjo@users.noreply.github.com>
2022-04-19 19:11:33 +0200

    Add files via upload

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mcloarec <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/11 12:17:30 by mcloarec          #+#    #+#             */
-/*   Updated: 2022/04/11 13:32:24 by mcloarec         ###   ########.fr       */
+/*   Created: 2022/04/19 17:47:17 by mcloarec          #+#    #+#             */
+/*   Updated: 2022/04/19 18:26:09 by mcloarec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,19 @@
 
 char	*ft_itoa(int n)
 {
-	int	len;
-	int	tmp;
-	char 	*str;
+	char	*str;
 
-	len = 0;
-	tmp = n;
-	if (n == - 2147483648)
-		return ("- 2147483648");
-	str = malloc(sizeof(char *) * (i + 1));
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	if (n < 0)
+		return (ft_strjoin("-", ft_itoa(-n)));
+	str = ft_strdup("0");
 	if (!str)
 		return (NULL);
-	str[i] = '\0';
-	if (n == 0)
-		str[0] = '0';
-		return (str);
-	if (n < 0)
-	{
-		len = len +  1;
-		n = n * -1;
-		str[0] = '-';
-	}
-	while (tmp)
-	{
-		tmp = tmp / 10;
-		len = len + 1;
-	}
-	while (n)
-	{
-		str[--len] = (n % 10) + '0';
-		n = n / 10;
-	}
-	return (str);
+	*str = *str + (n % 10);
+	if (n >= 0 && n < 10)
+		return (ft_strdup(str));
+	else
+		return (ft_strjoin(ft_itoa(n / 10), str));
+	return (0);
 }
--- libft/ft_split.c
+++ libft/ft_split.c
@@ -5,87 +5,77 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mcloarec <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/08 15:33:02 by mcloarec          #+#    #+#             */
-/*   Updated: 2022/04/08 18:39:12 by mcloarec         ###   ########.fr       */
+/*   Created: 2022/04/19 16:55:22 by mcloarec          #+#    #+#             */
+/*   Updated: 2022/04/19 17:28:33 by mcloarec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	count_words(char *s, char c)
+static int	ft_count_words(char const *s, char c)
 {
 	int	i;
-	int	letters;
+	int	count;
 
 	i = 0;
-	words = 0;
-	while (str[i] != '\0')
+	count = 0;
+	while (s[i] != '\0')
 	{
-		// pour aller au début d'un nouveau mot
-		if (str[i] != '\0' && str[i] == c)
-			i++;
-		else
-		{
-			letters++;
-			// pour aller au prochain séparateur
-			while (str[i] != '\0' && str[i] != c)
-				i++;
-		}
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+			count++;
+		i++;
 	}
-	return (letters);
+	return (count);
 }
 
-static char	put_words(char *s, char c)
+static int	ft_slen(char const *s, char c, int i)
 {
-	char 	*word;
-	int	i;
+	int	len;
 
-	i = 0;
-	while (s[i] != '\0' && s[i] != c)
-		i++;
-	word = malloc(sizeof(char *) * (i + 1));
-	if (!word)
-		return (NULL);
+	len = 0;
 	while (s[i] != '\0' && s[i] != c)
 	{
-		word[i] = s[i];
+		len++;
 		i++;
 	}
-	word[i] = '\0';
-	return (word);
-		
+	return (len);
+}
+
+static void	ft_free(char **s, int i)
+{
+	while (i > 0)
+	{
+		free (s[i]);
+		i--;
+	}
+	free (s);
+}
 
 char	**ft_split(char const *s, char c)
 {
-	int	i;
-	int	j;
-	int	words;
+	int		i;
+	int		j;
 	char	**tab;
 
-	if (!s)
-		return (NULL);
-	words = count_words((char *)s, c);
-	tab = malloc(sizeof(char **) * (words + 1));
-	if (!tab)
-		return (NULL);
 	i = 0;
 	j = 0;
-	while (s[i] != '\0')
+	tab = malloc(sizeof(char *) * (ft_count_words(s, c) + 1));
+	if (!tab)
+		return (NULL);
+	while (j < ft_count_words(s, c))
 	{
-		// va au début d'un nouveau mot
-		while (s[i] =! '\0' && s[i] == c)
+		while (s[i] == c)
 			i++;
-		if (s[i] =! '\0' && s[i] != c)
+		tab[j] = ft_substr(s, i, ft_slen(s, c, i));
+		tab[j][ft_slen(s, c, i)] = '\0';
+		if (!tab)
 		{
-			// écrit le mot dans une case du tableau
-			tab[j] = put_word(s);
-				i++;
-				j++;
-			// va au prochain séparateur
-			while (s[i] =! '\0' && s[i] != c)
-				i++;
+			ft_free(tab, j);
+			return (NULL);
 		}
+		i = i + ft_slen(s, c, i);
+		j++;
 	}
-	tab[j] = NULL
+	tab[j] = NULL;
 	return (tab);
 }
